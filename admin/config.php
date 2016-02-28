279a161c532c57626bb7d8b839944fe8ee8a472f
Shilan <shilan@shilan-HP-EliteBook-8440pexample.com>
2016-02-28 17:21:03 +0545

    Initial Upload

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/omslsbb/admin/');
+define('HTTP_CATALOG', 'http://localhost/omslsbb/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/omslsbb/admin/');
+define('HTTPS_CATALOG', 'http://localhost/omslsbb/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/omslsbb/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/omslsbb/system/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/omslsbb/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/omslsbb/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/omslsbb/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/omslsbb/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/omslsbb/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/omslsbb/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/omslsbb/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/omslsbb/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/omslsbb/system/storage/upload/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/omslsbb/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'omslsbb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/omslsbb/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/omslsbb/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/omslsbb/catalog/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/omslsbb/system/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/omslsbb/catalog/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/omslsbb/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/omslsbb/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/omslsbb/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/omslsbb/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/omslsbb/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/omslsbb/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/omslsbb/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/omslsbb/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'omslsbb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
